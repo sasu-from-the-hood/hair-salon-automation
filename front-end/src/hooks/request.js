fd6ae62b0931463f5544fc8a7d9623210dbb8605
incala <kalebcode25@gmail.com>
2024-09-22 21:22:45 +0300

    commit for to fix a bad push

---
+++ front-end/src/hooks/request.js
@@ -0,0 +1,46 @@
+
+const customFetch = async (url, options = {}) => {
+
+    if (!options.headers) {
+        options.headers = {};
+    }
+
+    const accessToken = sessionStorage.getItem('accessToken');
+    if (accessToken) {
+        options.headers['Authorization'] = `Bearer ${accessToken}`;
+    }
+
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message || 'An error occurred');
+    }
+
+    sessionStorage.setItem('accessToken', response.headers.get('Authorization').split(' ')[1]);
+    sessionStorage.setItem('refreshToken', response.headers.get('x-refresh-token'));
+
+    return response;
+};
+
+const getTokensFromApi = async (url = "/oauth/google/callback") => {
+    try {
+        const response = await fetch(url); // Make sure to replace with your endpoint
+        if (!response.ok) throw new Error('Failed to fetch tokens');
+
+        const data = await response.json();
+        if (data.token) {
+            sessionStorage.setItem('token', data.token);
+            sessionStorage.setItem('accessToken', JSON.stringify(data.accessToken));
+
+            window.location = data.Redirect
+        }
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+// On page load
+window.onload = () => {
+    getTokensFromApi();
+};
