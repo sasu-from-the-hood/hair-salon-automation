c97592796679e55f1969803287402a212e3cac73
abyssinia12 <abyssiniasolomon2123@gmail.com>
2024-09-28 23:06:22 +0300

    secondEdi

--- front-end/src/Landing page/LandingPage.jsx
+++ front-end/src/Landing page/LandingPage.jsx
@@ -2,11 +2,13 @@ import Explore from "./Explore";
 import Service from "./Service";
 import Footer from "./Footer";
 import { AiFillFacebook, AiFillInstagram, AiFillTikTok } from "react-icons/ai";
+import OurStory from "./OurStory";
 export default function LandingPage() {
   return (
     <div>
       <Explore />
       <Service />
+      <OurStory />
       <Footer />
       <div className="bg-orange-200 justify-center items-center flex pb-30 flex-row space-x-5 pb-2 text-3xl">
         <AiFillFacebook className="bg-browen-300 box-content px-2 py-1 rounded-3xl shadow-lg shadow-black/600 cursor-pointer hover:shadow-inner hover:shadow-black/300 duration-300 " />
