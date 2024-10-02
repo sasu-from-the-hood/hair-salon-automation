cace27f13c78a2d58694209df05c2a9111c003d4
abyssinia12 <abyssiniasolomon2123@gmail.com>
2024-10-02 15:33:11 +0300

    last

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -4,6 +4,7 @@ import SignUp from "./Authentication/SignUp";
 import PageNotExist from "./PageNotExist";
 import Admin from "./Admin/Admin";
 import LandingPage from "./Landing page/LandingPage";
+import { AiFillFacebook, AiFillInstagram, AiFillTikTok } from "react-icons/ai";
 export default function App() {
   return (
     <div>
@@ -15,6 +16,12 @@ export default function App() {
           <Route path="/admin/*" element={<Admin />} />
           <Route path="*" element={<PageNotExist />} />
         </Routes>
+
+        <div className="bg-orange-200 justify-center items-center flex pb-30 flex-row space-x-5 pb-2 text-3xl">
+          <AiFillFacebook className="bg-browen-300 box-content px-2 py-1 rounded-3xl shadow-lg shadow-black/600 cursor-pointer hover:shadow-inner hover:shadow-black/300 duration-300 " />
+          <AiFillInstagram className="bg-browen box-content px-2 py-1 rounded-3xl shadow-lg shadow-black/300 cursor-pointer hover:shadow-inner hover:shadow-black/300 duration-300 " />
+          <AiFillTikTok className="bg-browen-300 box-content px-2 py-1 rounded-3xl shadow-lg shadow-black/300 cursor-pointer hover:shadow-inner hover:shadow-black/400 duration-300 " />
+        </div>
       </Router>
     </div>
   );
---
+++ front-end/src/Landing page/Footer.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import icon from "../../public/footer-image/Vector.png";
+
+export default function Footer() {
+  return (
+    <>
+      <div className="last footer bg-orange-200 h-75 pt-10 mt-10 text-center">
+        <div className="flex justify-center mb-4">
+          <img src={icon} alt="icon" />
+        </div>
+        <div
+          className="flex justify-center text-bold text-4xl md:text-5xl"
+          style={{ fontFamily: '"Dancing Script", cursive' }}
+        >
+          <p className=" titel noto-serif px-1 font-bold">Gech</p>
+          <p className="text-xl noto-serif titel text-orange-400 md:text-2xl pt-3 font-bold">
+            Beauty Salon
+          </p>
+        </div>
+        <p
+          className="md:text-base text-sm pt-5 pb-3 font-bold"
+          style={{ fontFamily: '"Noto Serif", serif' }}
+        >
+          at Gech Hair Salon, your hair's health and beauty are our top
+          priorities. <br />
+          Whether you’re after a fresh new style or maintaining your current
+          look, we are committed to <br />
+          giving you a personalized and enjoyable salon experience that leaves
+          you feeling confident and refreshed.
+        </p>
+      </div>
+    </>
+  );
+}
