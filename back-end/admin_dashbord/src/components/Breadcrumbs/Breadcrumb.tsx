cdd6d4b021f449c70c6be09fc29a7152f3226aad
abyssinia12 <abyssiniasolomon2123@gmail.com>
2024-10-06 12:03:33 +0300

    first

--- back-end/admin_dashbord/src/components/Breadcrumbs/Breadcrumb.tsx
+++ back-end/admin_dashbord/src/components/Breadcrumbs/Breadcrumb.tsx
@@ -7,6 +7,7 @@ const Breadcrumb = ({ pageName }: BreadcrumbProps) => {
     <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
       <h2 className="text-title-md2 font-semibold text-black dark:text-white">
         {pageName}
+        
       </h2>
 
       <nav>
